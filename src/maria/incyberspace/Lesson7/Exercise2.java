a0b0f59804fba3b2b43e480e1386442d572e6686
Maria Spirid <lan194@hotmail.com>
2021-10-27 22:48:36 +0300

    Lesson 7, Exercise 2

--- src/maria/incyberspace/Lesson7/Exercise2.java
+++ src/maria/incyberspace/Lesson7/Exercise2.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class Exercise2 {
     public static void main(String[] args) {
         // Instantiate and initialize an array of Shape objects
-        Shape[] shapes = new Shape[] { new Ball(3, 5), new Pyramid(4, 2, 2),
-                new Cylinder(5, 3) };
+        Shape[] shapes = new Shape[] { new Ball(8), new Pyramid(4, 2),
+                new Cylinder(3, 3) };
         // Instantiate and initialize a Box object with a given volume
-        Box box = new Box(11);
+        Box box = new Box(2500);
         // Add the shapes into the box
         for (Shape shape : shapes) {
             box.add(shape);
@@ -22,10 +22,11 @@ public class Exercise2 {
 class Box extends Shape { // Container for shapes
 
     private double controlVolume; // A private field needed to store the total volume of the Shapes added to the box
+
     private ArrayList<Shape> shapes; // Instantiate an ArrayList of Shapes to put shapes into
 
-    public Box(double maxVol) {
-        super(maxVol); // Instantiates a new Shape object, initializes the 'volume' field with the given value that represents maximum volume of the box
+    public Box(double maxVol) { // Instantiates a new Shape object,
+        super.volume = maxVol; // initializes the 'volume' field pertinent to the superclass with the given value that represents maximum volume of the box
         shapes = new ArrayList<>(); // Initialize an ArrayList to put shapes into
     }
 
@@ -34,8 +35,10 @@ class Box extends Shape { // Container for shapes
     }
 
     public boolean add(Shape shape) {
-        controlVolume += shape.volume; // Update the controlVolume variable
-        if (controlVolume <= this.volume) { // Check that the volume of the box isn't exceeded
+        controlVolume += shape.getVolume(); // Update the controlVolume variable
+        // Check that the volume of the box isn't exceeded
+        if (controlVolume <= super.volume) { // by checking the volume field of our superclass we've initialized in the constructor
+            System.out.println(controlVolume);
             shapes.add(shape);
             return true;
         } else {
@@ -49,11 +52,7 @@ class Shape {
     protected double volume;
     protected String name = "Shape";
 
-    public Shape(double volume) {
-        this.volume = volume;
-    }
-
-    public double getVolume() {
+    protected double getVolume() {
         return volume;
     }
 
@@ -65,46 +64,59 @@ class Shape {
 }
 
 class Pyramid extends Shape {
-    double s;
-    double h;
-    final String name = "Pyramid";
+    private double baseArea;
+    private double height;
+    private final String name = "Pyramid";
 
-    public Pyramid(double volume, double s, double h) {
-        super(volume);
+    public Pyramid(double baseArea, double height) {
         super.name = name;
-        this.s = s;
-        this.h = h;
+        this.baseArea = baseArea;
+        this.height = height;
+        super.volume = this.getVolume();
+    }
+
+    @Override
+    public double getVolume() {
+        return (baseArea * height) / 3;
     }
 }
 
 class SolidOfRevolution extends Shape {
     protected double radius;
 
-    public SolidOfRevolution(double volume, double radius) {
-        super(volume);
-        this.radius = radius;
-    }
-
-    public double getRadius() {
+    protected double getRadius() {
         return radius;
     }
 }
 
 class Cylinder extends SolidOfRevolution {
-    double height;
-    final String name = "Cylinder";
+    private double height;
+    private final String name = "Cylinder";
 
-    public Cylinder(double volume, double radius) {
-        super(volume, radius);
+    public Cylinder(double radius, double height) {
+        super.radius = radius;
         super.name = name;
+        this.height = height;
+        volume = this.getVolume();
+    }
+
+    @Override
+    public double getVolume() {
+        return height * Math.PI * Math.pow(radius, 2);
     }
 }
 
 class Ball extends SolidOfRevolution {
-    final String name = "Ball";
+    private final String name = "Ball";
 
-    public Ball(double volume, double radius) {
-        super(volume, radius);
+    public Ball(double radius) {
+        super.radius = radius;
         super.name = name;
+        volume = this.getVolume();
+    }
+
+    @Override
+    public double getVolume() {
+        return (4 * Math.PI * Math.pow(radius, 3)) / 3;
     }
 }
